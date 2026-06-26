41a3d893f70bf1913769d563d560b7d6926f22bc
Particular_Catch_449 <jstinchcombe8642@gmail.com>
2026-06-26 08:40:51 +0100

    .

--- resources/youtube-channels.js
+++ resources/youtube-channels.js
@@ -6,6 +6,11 @@ module.exports = [
 		country: 'England'
 	},
 	{
+		channelId: 'UCCmo_NIuQR5eU4AvBa6sEQQ',
+		competition: 'ENGLAND: Championship',
+		country: 'England'
+	},
+	{
 		channelId: 'UCTv-XvfzLX3i4IGWAm4sbmA',
 		competition: 'SPAIN: La Liga',
 		country: 'Spain'
@@ -32,10 +37,46 @@ module.exports = [
 		country: 'France'
 	},
 	{
+		playlistId: 'PL_5dsSfLn2v-NzXumR_AYC370Zbhnnx4L',
+		channelId: 'UCXnPiEv1DoUCDAqDUXT9shQ',
+		competition: 'NETHERLANDS: Eredivisie',
+		country: 'Netherlands'
+	},
+	{
+		channelId: 'UCOY_cGXSOJdlYxdDP9PiBHQ',
+		competition: 'PORTUGAL: Primeira Liga',
+		country: 'Portugal'
+	},
+	{
+		channelId: 'UCcRmcwL72nv49euLzd6r_ow',
+		competition: 'SAUDI ARABIA: Pro League',
+		country: 'Saudi Arabia'
+	},
+	{
+		channelId: 'UC2-QiuM-ACuqU7MMO8THwCw',
+		competition: 'BRASIL: Serie A',
+		country: 'Brazil'
+	},
+	{
 		playlistId: 'PLcj4z4KsbIoXrLpj2pOVr_maRaxhW902-',
 		channelId: 'UCSZbXT5TLLW_i-5W8FZpFsg',
 		competition: 'USA: Major League Soccer',
 		country: 'USA'
+	},
+	{
+		channelId: 'UCM8nHnLlZ6SNnW3WYsp6WAQ',
+		competition: 'ARGENTINA: Primera Division',
+		country: 'Argentina'
+	},
+	{
+		channelId: 'UCA1I1ial5Q2vZPkcXnHQQmw',
+		competition: 'TURKEY: Super Lig',
+		country: 'Turkey'
+	},
+	{
+		channelId: 'UCak5ZEX4BjijJcf7fdppuIQ',
+		competition: 'KOREA REP.: K-League',
+		country: 'South Korea'
 	},
 	{
 		playlistId: 'PL6o95GY_ersEqx_RC0XckgUu67lzl1orv',
